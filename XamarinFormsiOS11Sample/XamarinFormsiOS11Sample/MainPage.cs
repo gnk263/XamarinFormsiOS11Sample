d76b57895374f6c500d44cf943177fd8803b4ccd
gnk263 <gnk263@users.noreply.github.com>
2017-11-15 00:49:30 +0900

    サンプルコードを追加

---
+++ XamarinFormsiOS11Sample/XamarinFormsiOS11Sample/MainPage.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace XamarinFormsiOS11Sample
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Title = "テスト画面";
+
+
+            // iOS用のSetUseSafeArea設定
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+
+
+            // ListView用のデータを作成
+            var listData = Enumerable.Range(1, 100)
+                                .Select(i => $"item {i}")
+                                .ToList();
+            
+            // ListViewを作成
+            var listView = new ListView
+            {
+                ItemsSource = listData,
+                BackgroundColor = Color.LightSkyBlue
+            };
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    listView
+                }
+            };
+        }
+    }
+}
